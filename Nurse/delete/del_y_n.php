988a5d97345ad336a7cf5753fa2c1a39dd4ea113
Himani-Trivedi <himanitrivedi1874@gmail.com>
2022-02-28 09:58:52 +0530

    Nurse withdraw amount added

--- Nurse/delete/del_y_n.php
+++ Nurse/delete/del_y_n.php
@@ -40,10 +40,17 @@ if (isset($_REQUEST['email'])) {
 
 <body>
     <div class="container rounded-3 my-5 p-4" id="confirm" style="width: 600px;  box-shadow:5px 10px rgba(63,187,192,255)">
-        <h3>Are you sure to delete your account?</h3>
+        <!-- <h3>Are you sure to delete your account?</h3>
         <a href="del_req_nurse.php?s=2" class="btn btn-secondary" style="background-color:rgba(63,187,192,255) ;">YES
         </a>
-        <a href='../system_nurses/profile.php' class="btn btn-secondary">NO</a>
+        <a href='../system_nurses/profile.php' class="btn btn-secondary">NO</a> -->
+
+        <h3>What do you want to do?</h3>
+        <br>
+        <a href='../system_nurses/withdraw_del.php' class="btn btn-secondary">Withdraw Amount</a>
+        <a href="del_req_nurse.php?s=2" class="btn btn-secondary" style="background-color:rgba(63,187,192,255) ;">Delete Account
+        </a>
+        <a href='../system_nurses/profile.php' class="btn btn-secondary">Cancel</a>
     </div>
 </body>
 

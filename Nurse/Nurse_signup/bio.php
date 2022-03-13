dbd6fe005ad7de5a232cc4cdb34269223c5ef015
Himani-Trivedi <himanitrivedi1874@gmail.com>
2022-03-13 11:03:48 +0530

    icon added

--- Nurse/Nurse_signup/bio.php
+++ Nurse/Nurse_signup/bio.php
@@ -3,8 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<title>Bio</title>
-    <link href="../logo.jpeg" rel="icon">
-
+    <link href="../../logo.jpeg" rel="icon">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="style.css">
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
